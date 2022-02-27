ecf14b037b09237f1d4352bd608b143fbfb0abde
hilalhmdy <13519024@std.stei.itb.ac.id>
2022-02-28 00:52:50 +0700

    add help

--- script.js
+++ script.js
@@ -259,3 +259,66 @@ const refreshChosenInfo = () => {
   document.getElementById('properti').innerHTML = toShow.rightDisplay() + toShow.uniqueDisplay();
 };
 refreshChosenInfo();
+
+
+var exportFile = function() {
+  var filename = document.getElementById("export_file").value
+
+  if (!filename) {
+      filename = 'data'
+  }
+
+  var data = JSON.stringify(arrObjects);
+  download(filename + ".json", data);
+
+  console.log("The file was saved!"); 
+}
+
+var importFile = function() {
+  var file = document.getElementById("import_file").files[0]
+  var reader = new FileReader();
+  // var data = [];
+  reader.onload = function(e){
+      console.log('file imported')
+      arrObjects = JSON.parse(e.target.result);
+      // console.log(data)
+      // arrObjects = data
+      renderAll()
+  }
+  
+  reader.readAsText(file);
+  if (!file) {
+      alert('Blank file')
+  }
+}
+
+var download = function(filename, text) {
+  var element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+  element.setAttribute('download', filename)
+
+  element.style.display = 'none'
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
+}
+
+var help = document.getElementById("help");
+var btn = document.getElementById("helpBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  help.style.display = "block";
+}
+
+span.onclick = function() {
+  help.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == help) {
+      help.style.display = "none";
+  }
+} 
