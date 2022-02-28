62ecc235ed9f39ba234d52a0adaf96f63b21417e
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2022-02-28 17:09:41 +0700

    fix: wrong starting point on line

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ canvas.addEventListener('mouseup', (e) => {
     drawMethod = '';
   } else if (drawMethod == 'Line') {
     drawMethod = 'Line2';
+    objects[objects.length - 1].vertices[0].coor = [x, y];
   } else if (drawMethod == 'Line2') {
     drawMethod = '';
   } else if (drawMethod == 'Square') {
@@ -267,65 +268,64 @@ const refreshChosenInfo = () => {
 };
 refreshChosenInfo();
 
-
-var exportFile = function() {
-  var filename = document.getElementById("export_file").value
+var exportFile = function () {
+  var filename = document.getElementById('export_file').value;
 
   if (!filename) {
-      filename = 'data'
+    filename = 'data';
   }
 
   var data = JSON.stringify(arrObjects);
-  download(filename + ".json", data);
+  download(filename + '.json', data);
 
-  console.log("The file was saved!"); 
-}
+  console.log('The file was saved!');
+};
 
-var importFile = function() {
-  var file = document.getElementById("import_file").files[0]
+var importFile = function () {
+  var file = document.getElementById('import_file').files[0];
   var reader = new FileReader();
   // var data = [];
-  reader.onload = function(e){
-      console.log('file imported')
-      arrObjects = JSON.parse(e.target.result);
-      // console.log(data)
-      // arrObjects = data
-      render()
-  }
-  
+  reader.onload = function (e) {
+    console.log('file imported');
+    arrObjects = JSON.parse(e.target.result);
+    // console.log(data)
+    // arrObjects = data
+    render();
+  };
+
   reader.readAsText(file);
   if (!file) {
-      alert('Blank file')
+    alert('Blank file');
   }
-}
+};
 
-var download = function(filename, text) {
-  var element = document.createElement('a')
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
-  element.setAttribute('download', filename)
+var download = function (filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
 
-  element.style.display = 'none'
-  document.body.appendChild(element)
+  element.style.display = 'none';
+  document.body.appendChild(element);
 
-  element.click()
+  element.click();
 
-  document.body.removeChild(element)
-}
+  document.body.removeChild(element);
+};
 
-var help = document.getElementById("help");
-var btn = document.getElementById("helpBtn");
-var span = document.getElementsByClassName("close")[0];
+var help = document.getElementById('help');
+var btn = document.getElementById('helpBtn');
+var span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function() {
-  help.style.display = "block";
-}
+btn.onclick = function () {
+  help.style.display = 'block';
+};
 
-span.onclick = function() {
-  help.style.display = "none";
-}
+span.onclick = function () {
+  help.style.display = 'none';
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == help) {
-      help.style.display = "none";
+    help.style.display = 'none';
   }
-} 
+};
